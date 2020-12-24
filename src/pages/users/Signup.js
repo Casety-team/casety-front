d6f5f56c0155a32a13018331efe0e51382f58860
MDrapala <drapalamathieu95@gmail.com>
2020-12-24 06:26:51 +0100

    feat: Update router for switch pages

--- src/pages/users/Signup.js
+++ src/pages/users/Signup.js
@@ -1,9 +1,24 @@
 import React from "react";
-
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value});  
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(event)
+  }
+
   render(){
     return (
-      <h1>Signup</h1>
+        <h1>Signup</h1>
     );
   }
 }
