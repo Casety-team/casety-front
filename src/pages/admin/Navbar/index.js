c7d05160fc248318ee1bedd22628d0d4388b8131
MDrapala <drapalamathieu95@gmail.com>
2021-06-22 12:54:39 +0200

    clear nav bar

--- src/pages/admin/Navbar/index.js
+++ src/pages/admin/Navbar/index.js
@@ -1,6 +1,9 @@
 const Navbar = (value) => {
   return (
-    <aside style={{marginTop: 80}} className="fixed top-0 bg-blue-500 h-screen w-64 hidden sm:block shadow-xl">
+    <aside
+      style={{ marginTop: 80 }}
+      className="fixed top-0 bg-blue-500 h-screen w-64 hidden sm:block shadow-xl"
+    >
       <nav className="text-white text-base font-semibold pt-3">
         <a
           href="/admin"
@@ -12,17 +15,6 @@ const Navbar = (value) => {
         >
           <i className="fas fa-tachometer-alt mr-3"></i>
           Dashboard
-        </a>
-        <a
-          href="/admin/users"
-          className={
-            value === "users"
-              ? "flex items-center active-nav-link text-white py-4 pl-6 nav-item"
-              : "flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item"
-          }
-        >
-          <i className="fas fa-sticky-note mr-3"></i>
-          Utilisateurs
         </a>
         <a
           href="/admin/blog"
